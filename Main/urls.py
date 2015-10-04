6af82c73d6b31d75a71e72f5de2e9fedf8abd87c
gideon-steinberg <gideon_steinberg@hotmail.com>
2015-10-04 13:43:16 +1300

    add robots.txt and homepage

--- Main/urls.py
+++ Main/urls.py
@@ -1,10 +1,20 @@
 from django.conf.urls import include, url
 from django.http import HttpResponseRedirect
+from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def redirect(request):
     return HttpResponseRedirect("/stocks/stocks")
 
+def robots(request):
+    return HttpResponse("<pre>User-agent: *\nDisallow: /</pre>")
+
+def main(request):
+    return render(request, 'home.html', {})
+
 urlpatterns = [
+    url(r'^robots.txt$', robots),          
     url(r'^stocks/', include('Stocks.urls')),
-    url(r'^.*$', redirect),
+    url(r'^.*$', main),
 ]
